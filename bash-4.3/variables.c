6dee94dd91363d4a2fd8cc09aea808af3e3c6bcf
ryan nelson <ryan@ryan.net>
2014-09-26 16:33:10 -0700

    added pkgsrc patches from here: http://pkgsrc.se/files.php?messageId=20140925202832.9AD9C98@cvs.netbsd.org

--- bash-4.3/variables.c
+++ bash-4.3/variables.c
@@ -105,6 +105,7 @@ extern time_t shell_start_time;
 extern int assigning_in_environment;
 extern int executing_builtin;
 extern int funcnest_max;
+extern int import_functions;
 
 #if defined (READLINE)
 extern int no_line_editing;
@@ -349,7 +350,7 @@ initialize_shell_variables (env, privmode)
 
       /* If exported function, define it now.  Don't import functions from
 	 the environment in privileged mode. */
-      if (privmode == 0 && read_but_dont_execute == 0 && STREQN ("() {", string, 4))
+      if (import_functions && privmode == 0 && read_but_dont_execute == 0 && STREQN ("() {", string, 4))
 	{
 	  string_length = strlen (string);
 	  temp_string = (char *)xmalloc (3 + string_length + char_index);
